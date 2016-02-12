6f976ef0d66d8848e3b1bf92721ecaed71210a7f
josuemontano <josuemontanoa@gmail.com>
2016-02-12 13:24:44 -0400

    Create ticket model

--- wrapper/models.py
+++ wrapper/models.py
@@ -2,3 +2,25 @@ class Requester:
     def __init__(self, name, email):
         self.name = name
         self.email = email
+
+
+class Ticket:
+    def __init__(self, id, subject, content, requester, starred=False, spam=False):
+        """
+        :param ticket_id: ID of the ticket
+        :param subject: Subject of the ticket. REQUIRED for posting data
+        :param starred: Starred
+        :param requester_email:  Specifies the email of the requester of the ticket. REQUIRED for posting data
+        :param content: Specifies the content of the ticket. Either text or html must be present.
+                        Please look at the example above. REQUIRED for posting data
+        :return:
+        """
+        self.id = id
+        self.subject = subject
+        self.content = content
+        self.requester = requester
+        self.starred = starred
+        self.spam = spam
+
+    def __eq__(self, other):
+        return self.id == other.id
--- wrapper/tests/test_models.py
+++ wrapper/tests/test_models.py
@@ -1,6 +1,6 @@
 import pytest
 
-from ..models import Requester
+from ..models import Requester, Ticket
 
 
 class TestRequester:
@@ -8,3 +8,27 @@ class TestRequester:
         requester = Requester("John", "Smith")
         assert requester.name == "John"
         assert requester.email == "Smith"
+
+
+class TestTicket:
+    def test_init(self):
+        requester = Requester("John", "Smith")
+        ticket = Ticket(1, "Subject", "Content", requester)
+        assert ticket.id == 1
+        assert ticket.subject == "Subject"
+        assert ticket.content == "Content"
+        assert ticket.requester == requester
+        assert ticket.starred == False
+        assert ticket.spam == False
+
+    def test_eq(self):
+        ticket_a = Ticket(1, None, None, None)
+        ticket_b = Ticket(1, None, None, None)
+
+        assert ticket_a == ticket_b
+
+    def test_not_eq(self):
+        ticket_a = Ticket(1, None, None, None)
+        ticket_b = Ticket(2, None, None, None)
+
+        assert ticket_a != ticket_b
